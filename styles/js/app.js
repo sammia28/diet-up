b32a32aa72cf3644ab4cd889c16c583826e837ae
Sammia Karen Zubizarreta Arimuya <sammia.zubizarreta@gmail.com>
2017-01-27 18:52:13 -0500

    conflictos solucionado

--- styles/js/app.js
+++ styles/js/app.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-	$(".option-li").click(function(){
-		$(".menu").css("display", "none");
-	});
-=======
 	$(".option-li").click(function(){  
 		$(".menu").css("display", "none");
 	});
 	$("#btn-menu").click(function(){
 		$(".menu").css("display", "block");
 	});
->>>>>>> master
 });
