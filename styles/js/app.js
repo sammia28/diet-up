d905c70ef9b2b57372737b79a488f9397cf2b865
Luis Pumaricra Diaz <luis.pumaricra@bongous.com>
2017-02-03 22:58:14 -0500

    update

--- styles/js/app.js
+++ styles/js/app.js
@@ -4,24 +4,67 @@ function main () {
 	$(".menu_bar").click(function(){
 		if (contador == 1) {
 			$("nav").animate({
-				top: "-100%"
+				top: "1%"
 			});
 			contador = 0;
 		} else {
 			contador = 1;
 			$("nav").animate({
-				top: "1%"
+				top: "-100%"
 			});
 		}
 	});
+
+    $( "#header-menu nav .menu-hide" ).click(function() {
+        $("nav").animate({
+            top: "-100%"
+        });
+        contador = 1;
+    });
+
 	$(window).scroll(function(){
-	    window_y = $(window).scrollTop();
-	    scroll_critical = parseInt($("#header-menu").height());
-	    if (window_y > scroll_critical) {
-	       $("#header-menu").css("background-color","#14c6ae");
-	    } else {
-	    	$("#header-menu").css("background-color","transparent");
-	    }
+	    var window_y = $(window).scrollTop();
+	    var scroll_critical = parseInt($("#header-menu").height());
+        if( !isMobile.any() ){
+            if (window_y > scroll_critical) {
+                $("#header-menu").css("background-color","#FFF");
+                $("header nav").addClass("navJS");
+
+                $(".header-logo").css("background-color", "transparent");
+                $(".header-logo").css("padding-top", "0px");
+                $(".header-logo").css("top", "2px");
+                $(".logo-diet-up").css("padding-top", "5px");
+
+            } else if(window_y <= scroll_critical) {
+                $("#header-menu").css("background-color","transparent");
+                $("header nav").removeClass("navJS");
+
+                $(".header-logo").css("background-color", "#FFF");
+                $(".header-logo").css("padding-top", "20px");
+                $(".header-logo").css("top", "15px");
+                $(".logo-diet-up").css("padding-top", "0px");
+            }
+        }
 	});
 }
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
